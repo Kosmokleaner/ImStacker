63e581ab7c401024bf1046edbfd46dd51e6d59ad
Martin Mittring <Kosmokleaner@Kosmokleaner.de>
2022-04-29 18:26:09 -0700

    emscripen compiles without shaderbox

--- ShaderBox.cpp
+++ ShaderBox.cpp
@@ -1,10 +1,44 @@
 #include "imgui.h"
 #include <string>
+#include "ShaderBox.h"
+
+#ifdef EMSCRIPTEN
+  #define IMGL3W_IMPL
+#endif
+
 #include <imgui_impl_opengl3_loader.h>
 
-// https://www.khronos.org/registry/OpenGL/api/GL/glext.h
-#define GL_STATIC_DRAW                    0x88E4
+#ifdef EMSCRIPTEN
+const char* vertex_shader_text =
+"#version 110\n"
+"attribute vec3 aPos; // the position variable has attribute position 0\n"
+"\n"
+"out vec4 vertexColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  gl_Position = vec4(aPos, 1.0);\n"
+"  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"}\n"
+"\n";
 
+const char* fragment_shader_text0 =
+"#version 110\n"
+"#ifdef GL_ES\n"
+"    precision mediump float;\n"
+"#endif\n"
+"\n"
+"uniform mat4 uniform0;\n"
+"in vec4 vertexColor; // from vertex shader\n"
+"in vec4 gl_FragCoord; // (pixel.x+0.5, pixel.y+0.5, z, w)\n"
+"\n"
+"void main()\n"
+"{\n"
+"  uvec2 checker2 = uvec2(gl_FragCoord.xy / 16.0);\n"
+"  uint checker1 = (checker2.x & 1u) ^ (checker2.y & 1u);\n"
+"  FragColor = vec4(1.0, 1.0f, 1.0, 1.0) * mix(0.45f, 0.55f, checker1);\n\n";
+
+#else
 const char* vertex_shader_text =
 "#version 330 core\n"
 "layout(location = 0) in vec3 aPos; // the position variable has attribute position 0\n"
@@ -31,10 +65,22 @@ const char* fragment_shader_text0 =
 "  uvec2 checker2 = uvec2(gl_FragCoord.xy / 16.0);\n"
 "  uint checker1 = (checker2.x & 1u) ^ (checker2.y & 1u);\n"
 "  FragColor = vec4(1.0, 1.0f, 1.0, 1.0) * mix(0.45f, 0.55f, checker1);\n\n";
+#endif
 
 const char* fragment_shader_text1 =
 "}\n"
 "\n";
+
+
+#if SHADER_SUPPORT == 1
+
+// https://www.khronos.org/registry/OpenGL/api/GL/glext.h
+#define GL_STATIC_DRAW                    0x88E4
+
+//#define IMGUI_IMPL_OPENGL_ES3               // iOS, Android  -> GL ES 3, "#version 300 es"
+//#define IMGUI_IMPL_OPENGL_ES2               // Emscripten    -> GL ES 2, "#version 100"
+
+// but for gl_FragCoord we need more than 100, see https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/gl_FragCoord.xhtml
 
 static GLuint vertex_shader = (GLuint)-1;
 static GLuint g_program = (GLuint)-1;
@@ -42,6 +88,7 @@ static GLuint g_vbo = (GLuint)-1;
 static GLuint g_idx = (GLuint)-1;
 // 0 is a valid uniform index, -1 can mean it was compiled out
 static GLuint uniform0 = (GLuint)-1;
+
 
 // If you get an error please report on GitHub. You may try different GL context version or GLSL version.
 // @param whatToCheck GL_LINK_STATUS for program or GL_COMPILE_STATUS for shaders
@@ -56,6 +103,10 @@ static bool checkProgram(GLuint handle, GLuint whatToCheck, const char* desc, st
     OutputDebugStringA("ERROR: ");
     OutputDebugStringA(desc);
     OutputDebugStringA("\n");
+#else
+    fprintf(stderr, "ERROR: ");
+    fprintf(stderr, "%s", desc);
+    fprintf(stderr, "\n");
 #endif
   }
   if (log_length > 1)
@@ -65,6 +116,8 @@ static bool checkProgram(GLuint handle, GLuint whatToCheck, const char* desc, st
     glGetProgramInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
 #ifdef WIN32
     OutputDebugStringA(buf.begin());
+#else
+    fprintf(stderr, "%s", buf.begin());
 #endif
     warningsAndErrors = buf.begin();
   }
@@ -176,3 +229,9 @@ void drawDemo() {
   //    ImGui::Text("Test");
   //    ImGui::End();
 }
+
+#else // SHADER_SUPPORT == 1
+void drawDemo() {}
+void recompileShaders(const char* /*inCode*/, std::string& /*warningsAndErrors*/) {}
+void deinit() {}
+#endif // SHADER_SUPPORT == 1
--- ShaderBox.h
+++ ShaderBox.h
@@ -3,6 +3,8 @@
 extern const char* fragment_shader_text0;
 extern const char* fragment_shader_text1;
 
+// 0:for testing, 1: default
+#define SHADER_SUPPORT 1
 
 // @param inCode must not be 0
 void recompileShaders(const char* inCode, std::string &warningsAndErrors);
